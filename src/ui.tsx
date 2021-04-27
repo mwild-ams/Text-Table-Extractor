fb9d2255442079dfab0c9a163757432b08aead02
mwild-ams <wild@adesso-mobile.de>
2021-04-27 17:16:51 +0200

    Added Prefix/Suffix inputs, removed other stuff except buttons

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import './ui.css'
+// import './ui.css'
 import '../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css'
 
 declare function require(path: string): any
@@ -23,10 +23,21 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>
-      <img src={require('./logo.svg')} />
-      <h2>Rectangle Creator</h2>
-      <p className="label">Count: <input ref={this.countRef} className="input__field"/></p>
+    return <div className="p-xxsmall">
+      {/* <img src={require('./logo.svg')} /> */}
+      {/* <h2>Rectangle Creator</h2> */}
+      <div className="radio">
+        <input type="radio" id="prefix" name="id-fix" value="prefix" className="radio__button" defaultChecked/>
+        <label htmlFor="prefix" className="radio__label">Prefix</label>
+        <input type="radio" id="suffix" name="id-fix" value="suffix" className="radio__button"/>
+        <label htmlFor="suffix" className="radio__label">Suffix</label>
+      </div>
+      <div className="input inline-flex">
+        <label htmlFor="id-identifier" className="label inline-flex">Prefix/Suffix</label>
+        <input type="text" id="id-identifier" defaultValue="*" placeholder="Please enter prefix/suffix." className="input__field inline-flex"/>
+      </div>
+
+      {/* <p className="label">Count: <input ref={this.countRef} className="input__field"/></p> */}
       <button id="create" onClick={this.onCreate} className='button button--primary'>Create</button>
       <button onClick={this.onCancel} className='button button--secondary'>Cancel</button>
     </div>
